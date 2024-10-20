bf1c65827a108746421ee4f213fcb8c24bce4a43
Yugesh Velayudhem Ganesh <yvel0002@monash.student.edu>
2024-10-20 19:54:42 +1100

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,8 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes: [
+    // your routes here
+  ]
+})
